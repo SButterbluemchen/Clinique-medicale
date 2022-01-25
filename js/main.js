a9fcb19b68304085dbfcb5c9a0735dc10c616e8f
SinaBrauch <sina.brauch@gmail.com>
2022-01-25 14:15:44 +0100

    navbar finished for mobile, desktop and tablet for index, shoulder and contact; html, css and js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+// -----------------------------------------------------------------------------
+// RETURN TO TOP BUTTON
+// -----------------------------------------------------------------------------
+
 //Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 250px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -11,9 +15,72 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// -----------------------------------------------------------------------------
+// NAVBAR
+// -----------------------------------------------------------------------------
+// Toggle mobile menu
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='far fa-times-circle'></i>";
+  }
+}
+
+//image instead of fontawesome as a burger menu
+// function toggleMenu() {
+//   if (menu.classList.contains("active")) {
+//     menu.classList.remove("active");
+//     toggle.querySelector("img").style.width = '100%';
+//     toggle.querySelector("img").src = "img/burger-menu.png";
+//   } else {
+//     menu.classList.add("active");
+//     toggle.querySelector("img").style.width = '0.8em';
+//     toggle.querySelector("img").src = "img/cancel-copy.png";
+//   }
+// }
+
+toggle.addEventListener('click', toggleMenu, false);
+
+// Submenu
+const items = document.querySelectorAll('.item');
+
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+for (let item of items) {
+  if (item.querySelector('.submenu')) {
+    item.addEventListener('click', toggleItem, false);
+    item.addEventListener('keypress', toggleItem, false);
+  }
+}
+
+// Close submenu from anywhere in the page
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector('.submenu-active')) {
+    menu.querySelector('.submenu-active').classList.remove('submenu-active');
+  }
+}
+
+document.addEventListener('click', closeSubmenu, false);
